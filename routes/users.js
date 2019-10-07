7ed4b14e434c612ca6868c4fec58fab6464e0997
Avinash jain <jainavinash881@gmail.com>
2019-10-07 09:53:02 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs'); 
+const {check, validationResult} = require('express-validator');
+
+const User = require('../modals/User');
+
+router.post('/',[
+    check('name','name is required').not().isEmpty(),
+    check('email','not a valid email ').isEmail(),
+    check('password','enter a password with atleast 6 charatcter').isLength({
+        min: 6
+    })
+],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});        
+    } 
+    const {name, email, password} = req.body;
+
+    try {
+        let user = await User.findOne({emai:email});
+        if(user){
+            return res.status(400).json({msg:'user already exist'});
+        }
+        user = new User({
+            name:name,
+            email:email,
+            password:password
+        });
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.sand('user saved');
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+ }
+);
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//db connection
+connectDB();
+
+//middelware
+app.use(express.json({extended:false}));
+
+app.get('/',(req, res)=> res.json({msg: "hello welcome"}));
+
+
+app.use('/api/users', require('./routes/users'));
+app.use('./api/auth',require('./routes/auth'));
+app.use('./api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server @Port:${PORT} `));
